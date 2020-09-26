552b3d774d482e819f5672b690b2ab76cb12020f
hushun1214 <2375353463@qq.com>
2020-09-26 18:02:31 +0800

    deploy

---
+++ precache-manifest.723fd65c85e7ca10d1970a25914d739b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58397934532997b9d81",
+    "url": "/Vue-morney-website/css/app.2b18d359.css"
+  },
+  {
+    "revision": "b90af0089605070dd490d5d6128cd5b4",
+    "url": "/Vue-morney-website/index.html"
+  },
+  {
+    "revision": "d58397934532997b9d81",
+    "url": "/Vue-morney-website/js/app.dc6f4e3e.js"
+  },
+  {
+    "revision": "da03e6c4c9bfd44c6f02",
+    "url": "/Vue-morney-website/js/chunk-vendors.8b48ead3.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Vue-morney-website/manifest.json"
+  },
+  {
+    "revision": "219b0ff31c3177191cd782d60aa46b51",
+    "url": "/Vue-morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-morney-website/robots.txt"
+  }
+]);
